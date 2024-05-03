ceede6e2b5ca8207372cab988346ad456eec451d
Luis Carlos Almenarez <luisalmenarez0428@gmail.com>
2024-05-03 17:25:16 -0500

    ⤴️Firts commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,18 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'my-app';
+  public title: string = 'Counter Angular';
+  public counter: number = 0;
+
+  increaseBy(value: number): void {
+    if (value < 0 && this.counter <= 0) return;
+    this.counter += value;
+  }
+
+  resetBy(): void {
+    this.counter = 0;
+  }
 }
