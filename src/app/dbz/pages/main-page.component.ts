6c0b871f126da921ee7c9ef52382ae63262ef140
Luis Carlos Almenarez <luisalmenarez0428@gmail.com>
2024-05-08 13:59:25 -0500

    ⚙️ Update

--- src/app/dbz/pages/main-page.component.ts
+++ src/app/dbz/pages/main-page.component.ts
@@ -20,4 +20,9 @@ export class MainPageComponent {
       power: 7500,
     },
   ];
+
+  onNewCharacter(character: Character): void {
+    console.log('Main Page');
+    console.log(character);
+  }
 }
